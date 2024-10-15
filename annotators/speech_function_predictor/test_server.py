bf0d8d64d895cdc751969bdd288269cdf705ec12
Lida Ostyakova <losta15@gpu4.ipavlov.mipt.ru>
2024-10-15 13:24:24 +0300

    Added tests for sf prediction

--- annotators/speech_function_predictor/test_server.py
+++ annotators/speech_function_predictor/test_server.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+import allure
 
 SERVICE_PORT = os.getenv("SERVICE_PORT")
 URL = f"http://0.0.0.0:{SERVICE_PORT}"
@@ -8,21 +8,30 @@ MODEL_URL = f"{URL}/model"
 ANNOTATION_URL = f"{URL}/annotation"
 
 
-def run_test():
-    model_test_data = ["Reply.Acknowledge"]
-    model_hypothesis = requests.post(MODEL_URL, json=model_test_data).json()
+def validate_input(input_data):
+    assert isinstance(input_data, list), "Input data must be a list"
+    assert all(isinstance(item, str) for item in input_data), "All elements in the list must be strings"
 
+
+@allure.description("Test model hypothesis and check that input is a list of strings")
+def test_model_hypothesis():
+    model_test_data = ["Reply.Acknowledge"]
+    validate_input(model_test_data)
+    model_hypothesis = requests.post(MODEL_URL, json=model_test_data).json()
     print("test name: sfp model_hypothesis")
     assert model_hypothesis == [{}]
 
-    annotation_test_data = ["Reply.Acknowledge"]
-    annotation_hypothesis = requests.post(ANNOTATION_URL, json=annotation_test_data).json()
 
+@allure.description("Test annotation hypothesis and check that input is a list of strings")
+def test_annotation_hypothesis():
+    annotation_test_data = ["Reply.Acknowledge"]
+    validate_input(annotation_test_data)
+    annotation_hypothesis = requests.post(ANNOTATION_URL, json=annotation_test_data).json()
     print("test name: sfp annotation_hypothesis")
     assert annotation_hypothesis == [{"batch": [{}]}]
 
-    print("Success")
-
 
 if __name__ == "__main__":
-    run_test()
+    test_model_hypothesis()
+    test_annotation_hypothesis()
+    print("Success")
