a0c64f666a92c8bdc8098cf6ca822de42ffe6cdb
Alexandra <alexandra.dolidze@gmail.com>
2024-10-27 03:41:34 +0300

    voice_service fixes

--- services/voice_service/test.py
+++ services/voice_service/test.py
@@ -1,23 +1,3 @@
-# import requests
-
-
-# def test_respond():
-#     url = "http://0.0.0.0:8333/respond"
-
-#     # voice_path = "..."
-
-#     contexts = [["hi", "hi. how are you?"], ["let's chat about movies", "cool. what movies do you like?"]]
-#     gold_result = [["Reacting to a voice message "]]
-#     result = requests.post(url, json={"utterances_histories": contexts}).json()
-#     assert [
-#         len(sample[0]) > 0 and all([len(text) > 0 for text in sample[0]]) and all([conf > 0.0 for conf in sample[1]])
-#         for sample in result
-#     ], f"Got\n{result}\n, but expected:\n{gold_result}"
-
-
-# if __name__ == "__main__":
-#     test_respond()
-
 import requests
 import time
 # import subprocess
@@ -30,8 +10,8 @@ URL = "http://0.0.0.0:8333/respond"
 @allure.description("""4.1.2 Test input and output data types""")
 @pytest.mark.parametrize("test_in_out_data", [
     {
-        "sound_paths": ["http://files:3000/file?file=file_50.wav"], #TODO: change path
-        "sound_durations": [5],
+        "sound_paths": ["http://files:3000/file?file=file_245.wav"], #TODO: change path
+        "sound_durations": [59],
         "sound_types": ['wav']
     },
     {
