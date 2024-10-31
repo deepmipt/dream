e713e7fd38981e64a9eaeb94721d3f6bd6bd0edf
screemix <chepurova@deeppavlov.ai>
2024-10-31 22:41:08 +0300

    improved text-qa service

--- services/text_qa/server.py
+++ services/text_qa/server.py
@@ -4,21 +4,15 @@ import time
 from flask import Flask, request, jsonify
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
-from deeppavlov import build_model
+from reader import Reader
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 
-language = os.getenv("LANGUAGE", "EN")
-config_name = os.getenv("CONFIG")
-
 try:
-    qa = build_model(config_name, download=True)
-    if language == "EN":
-        test_res = qa(["What is the capital of Russia?"], [["Moscow is the capital of Russia."]])
-    else:
-        test_res = qa(["Какая столица России?"], [["Москва - столица России."]])
+    reader = Reader()
+    test_res = reader.answer_question("What is the capital of Russia?", ["Capital of Russia"], ["Moscow is the capital of Russia."])
     logger.info("model loaded, test query processed")
 except Exception as e:
     sentry_sdk.capture_exception(e)
@@ -31,15 +25,21 @@ app = Flask(__name__)
 @app.route("/model", methods=["POST"])
 def respond():
     questions = request.json.get("question_raw", [" "])
-    facts = request.json.get("top_facts", [[" "]])
+    top_facts = request.json.get("top_facts", [[" "]])
+    logger.info(top_facts)
+
+    fact_titles = [[fact.split(";")[0].strip() for fact in facts] for facts in top_facts]
+    logger.info(fact_titles)
+
+    fact_texts = [[fact.split(";")[1].strip() for fact in facts] for facts in top_facts]
+
+    logger.info(fact_texts)
 
     qa_res = [["", 0.0, 0, ""] for _ in questions]
     try:
         tm_st = time.time()
-        qa_res = qa(questions, facts)
-        qa_res = [[elem[i] for elem in qa_res] for i in range(len(qa_res[0]))]
-        for i in range(len(qa_res)):
-            qa_res[i][1] = float(qa_res[i][1])
+        for i, question in enumerate(questions):
+            qa_res[i] = reader.answer_question(question, fact_titles[i], fact_texts[i])
         logger.info(f"text_qa exec time: {time.time() - tm_st}")
     except Exception as e:
         sentry_sdk.capture_exception(e)
