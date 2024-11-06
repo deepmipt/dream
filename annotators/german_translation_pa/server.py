457340eb4786ebd79a93475e6577d6b5a63ed606
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2024-11-06 20:30:30 +0400

    german translation pa added to dream_voice and dream_multimodal (working)

--- annotators/german_translation_pa/server.py
+++ annotators/german_translation_pa/server.py
@@ -1,37 +1,55 @@
 import os
-import requests
 import logging
+from typing import List
 
 from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from typing import List
+
+from gigachat import GigaChat
+from gigachat.models import Chat
 
 app = FastAPI()
 
-GIGACHAT_API_URL = os.getenv("GIGACHAT_API_URL", "http://gigachat-api:8187/respond")
-
 class TextInput(BaseModel):
-    last_human_annotated_utterance: List[str]
+    sentences: List[str]
 
-if not any(os.getenv("GIGACHAT_CREDENTIAL"), os.getenv("GIGACHAT_SCOPE")):
+if not all([os.getenv("GIGACHAT_CREDENTIAL"), os.getenv("GIGACHAT_SCOPE")]):
     logging.error("ENV VARIABLES FOR GIGACHAT ARE NOT SET, THE SERVICE WILL NOT WORK")
 
+@app.post("/translate")
+def translate_text(payload: TextInput):
+    gigachat_api_key = os.getenv("GIGACHAT_CREDENTIAL")
+    gigachat_org = os.getenv("GIGACHAT_SCOPE")
 
-@app.post("/translate") # last_human_annotated_utterance
-def translate_text(input: TextInput):
-    data = {
-        "dialog_contexts": [input.last_human_annotated_utterance],
-        "prompts": ["You are a translator that translates English text into German."],
-        "configs": [None],
-        "gigachat_credentials": [os.getenv("GIGACHAT_CREDENTIAL")],
-        "gigachat_scopes": [os.getenv("GIGACHAT_SCOPE")],
-    }
-    try:
-        response = requests.post(GIGACHAT_API_URL, json=data)
-        response.raise_for_status()
-        result = response.json()
-        logging.info(f"Translated text: {result}")
-        translated_text = result[0][0]
-        return {"translated_text": translated_text}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    if not all([gigachat_api_key, gigachat_org]):
+        logging.error("Gigachat credentials are not set")
+        raise HTTPException(status_code=500, detail="Gigachat credentials are not set")
+
+    translated_text = []
+
+    for msg in payload.sentences:
+        try:
+            giga = GigaChat(credentials=gigachat_api_key, verify_ssl_certs=False)
+
+            messages = [
+                {
+                    "role": "system",
+                    "content": "You are a translator that translates English text into German."
+                },
+                {
+                    "role": "user",
+                    "content": msg
+                }
+            ]
+
+            payload = Chat(messages=messages, scope=gigachat_org)
+
+            response = giga.chat(payload)
+
+            translated_text += [response.choices[0].message.content.strip()]
+            logging.info(f"Translated text: {translated_text}")
+        except Exception as e:
+            logging.exception("Error during translation")
+            raise HTTPException(status_code=500, detail=str(e))
+    return jsonable_encoder([{"batch": translated_text}])
