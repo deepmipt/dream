338c88e307a297d930c3efd30cca9b872960244f
screemix <chepurova@deeppavlov.ai>
2024-11-12 20:24:08 +0300

    added dataset downloading and env initializing in the test script

--- tests/text_qa_tests.sh
+++ tests/text_qa_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+wget http://files.deeppavlov.ai/chepurova/datasets/NQ-open.dev.jsonl  -O tests/NQ-open.dev.jsonl
 docker-compose -f docker-compose.yml -f assistant_dists/dream/docker-compose.override.yml -f assistant_dists/dream/dev.yml up  --detach --build fact-retrieval text-qa combined-classification > build.log 2>&1
-# source venv/bin/activate
+python3 -m venv test_venv
+source test_venv/bin/activate
+pip install -r tests/test_text_qa_requirements.txt 
 pytest -s --log-cli-level=INFO tests/test_text_qa_full_pipeline.py | tee tests.log
 docker-compose -f docker-compose.yml -f assistant_dists/dream/docker-compose.override.yml -f assistant_dists/dream/dev.yml down > service_stopping.log 2>&1
