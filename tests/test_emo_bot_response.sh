8cd443631f4b573bb53e0ee6ad0ceab314b5fef5
Daria Olshevskaya <olshevskaya.d@bk.ru>
2024-11-13 12:48:15 +0300

    fix: tests

--- tests/test_emo_bot_response.sh
+++ tests/test_emo_bot_response.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml -f assistant_dists/dream_emotion/docker-compose.override.yml -f assistant_dists/dream_emotion/dev.yml -p bot_test up --build -d openai-api-chatgpt emotional-bot-response
-python annotators/emotional_bot_response/test_launch_time.py
-python annotators/emotional_bot_response/test_time.py
-python annotators/emotional_bot_response/test_format.py
+cd annotators/emotional_bot_response
+python test_launch_time.py
+python test_time.py
+python test_format.py
+cd ../../
 docker-compose -f docker-compose.yml -f assistant_dists/dream_emotion/docker-compose.override.yml -f assistant_dists/dream_emotion/dev.yml -p bot_test down
--- tests/test_pers_detection.sh
+++ tests/test_pers_detection.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test up --build -d personality-detection
-python annotators/personality_detection/test_launch_time.py
-python annotators/personality_detection/test_time.py
-python annotators/personality_detection/test_format.py
-python annotators/personality_detection/test_accuracy.py
+cd annotators/personality_detection
+python test_launch_time.py
+python test_time.py
+python test_format.py
+python test_accuracy.py
+cd ../../
 docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test down
