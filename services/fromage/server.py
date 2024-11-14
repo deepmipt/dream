b5079a963b72a98f70a1dc5aa947df29b582f61f
anna-a-m <55382523+anna-a-m@users.noreply.github.com>
2024-11-14 12:17:12 +0300

    Add async

--- services/fromage/server.py
+++ services/fromage/server.py
@@ -3,15 +3,21 @@ import os
 import time
 import torch
 import sentry_sdk
-from flask import Flask, request, jsonify
-from sentry_sdk.integrations.flask import FlaskIntegration
+import asyncio
+import json
 
 from PIL import Image
 import requests
 from torchvision import transforms
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+from typing import List
+
+sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"))
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -30,8 +36,28 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-app = Flask(__name__)
 logging.getLogger("werkzeug").setLevel("WARNING")
+
+STATE_FILE = "task_state.json"
+
+
+def read_task_state():
+    try:
+        with open(STATE_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {"task_state": ""}
+
+    
+def write_task_state(state):
+    with open(STATE_FILE, "w") as file:
+        json.dump(state, file)
+
+
+class FromagePayload(BaseModel):
+    image_paths: List[str] = []
+    sentences: List[str] = []
+
 
 def generate_responses(image_path, prompt):
     image = Image.open(requests.get(image_path, stream=True).raw).convert('RGB')
@@ -44,13 +70,8 @@ def generate_responses(image_path, prompt):
     return [model_answer]
 
 
-@app.route("/respond", methods=["POST"])
-def respond():
-    st_time = time.time()
-    image_paths = request.json.get("image_paths")
-    sentences = request.json.get("sentences")
-
-    # frmg_answers = []
+async def subinfer(image_paths, sentences):
+    write_task_state({"task_state": "scheduled"})
     for image_path, sentence in zip(image_paths, sentences):
         if image_path and image_path.lower().endswith(('.png', '.jpg', '.jpeg')):
             try:
@@ -61,9 +82,38 @@ def respond():
                 frmg_answers = [""]
         else:
             frmg_answers = [""]
+    write_task_state({"task_state": "done", "response": frmg_answers})
+    logger.info(f"VOICE_SERVICE RESPONSE: {frmg_answers}")
+
+            
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)            
+            
+
+@app.post("/respond")
+async def respond(payload: FromagePayload):
+    st_time = time.time()
+    responses = []
+    task_state = read_task_state()
+    logger.info(f"Task state: {task_state}")
+    
+    if not task_state.get("task_state") and payload.image_paths:
+        asyncio.create_task(subinfer(payload.image_paths, payload.sentences))
+        responses.append({"task_state": "scheduled"})
+        
+    if task_state.get("task_state") == "scheduled":
+        responses.append(task_state)
+    elif task_state.get("task_state") == "done":
+        responses.append(task_state)
+        write_task_state({"task_state": ""})
 
     total_time = time.time() - st_time
-    logger.info(f"fromage results: {frmg_answers}")
     logger.info(f"fromage exec time: {total_time:.3f}s")
-    logger.info(jsonify(frmg_answers))
-    return jsonify(frmg_answers)
+    return jsonable_encoder(responses)
+
