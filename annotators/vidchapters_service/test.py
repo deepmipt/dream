03919572a1734c2214ab7be3dbe30f38b443a039
Alexandra <alexandra.dolidze@gmail.com>
2024-11-21 00:49:51 +0300

    vidchapters tests fixed

--- annotators/vidchapters_service/test.py
+++ annotators/vidchapters_service/test.py
@@ -4,41 +4,73 @@ import subprocess
 import allure
 import json
 
-url = "http://0.0.0.0:8045/respond"
+URL = "http://0.0.0.0:8045/respond"
+DUMMY_JSON = {"video_paths": ["http://files:3000/file?file=non_existent.mp4"]}
+
+def _call_service(payload):
+    time_deltas = []
+    start_time = time.time()
+    result = requests.post(URL, json=payload)
+    stop_time = time.time()
+    time_deltas.append(stop_time - start_time)
+    
+    result = result.json()
+    my_task_id = result.get("task_id")
+
+    caption = "Error"
+    for _attempt in range(100):
+        start_time = time.time()
+        result = requests.post(URL, json=DUMMY_JSON)
+        stop_time = time.time()
+        time_deltas.append(stop_time - start_time)
+        result = result.json()
+        all_tasks_info = result.get('all_status')
+        for t in all_tasks_info:
+            if t['id'] == my_task_id:
+                    current_task_info = t 
+        if current_task_info['status'] == "completed": 
+            caption = current_task_info.get('caption')
+            caption = caption[0]['caption']
+
+            assert isinstance(result, (dict, list)), "Expected result to be a JSON object or array"
+
+            break
+        else:
+            time.sleep(10)
+    
+    avg_response_time = sum(time_deltas) / len(time_deltas)
+    return caption, avg_response_time
 
 @allure.description("""4.1.2 Test input and output data types""")
 def test_in_out():
     video_path = "https://raw.githubusercontent.com/deeppavlov/mmodal_files_bkp/refs/heads/main/medals.mp4"
-    test_data = { "video_paths": [video_path], "video_durations": [59], "video_types": ['.mp4']}
-    result = requests.post(url, json=test_data)
-    while result.json() and not result.json()[0].get("response"):
-        result = requests.post(url, json={})
+    test_data = { 
+        "video_paths": [video_path], 
+        "video_durations": [59], 
+        "video_types": ['.mp4']
+    }
     valid_extensions = ['.mp4']
-    assert any(test_data["video_paths"][0].lower().endswith(ext) for ext in valid_extensions), "Invalid input type"
-    assert isinstance(result.json(), (dict, list)), "Expected result to be a JSON object or array"
-    print(f"...\nSent file {video_path},\ngot response {result.json()[0].get("response")}")
-
+    for path in test_data.get("video_paths"):
+        if path:
+            assert any(path.lower().endswith(ext) for ext in valid_extensions), "Invalid input type"
+            print(f"...\nSent file {test_data.get('video_paths')},\ngot correct input type") #TODO which format get
+    caption, _  = _call_service(test_data)
+    print(f"...\nSent file {test_data.get('video_paths')},\ngot response {caption}")  
+    # assert any(test_data["video_paths"][0].lower().endswith(ext) for ext in valid_extensions), "Invalid input type"
+    # assert isinstance(result.json(), (dict, list)), "Expected result to be a JSON object or array"
+    # print(f"...\nSent file {video_path},\ngot response {result.json()[0].get("response")}")
 
 @allure.description("""4.1.3 Test execution time""")
 def test_exec_time():
     video_path = "https://raw.githubusercontent.com/deeppavlov/mmodal_files_bkp/refs/heads/main/medals.mp4"
-    test_data = { "video_paths": [video_path], "video_durations": [59], "video_types": ['.mp4']}    
-    start_time = time.time()
-    result = requests.post(url, json=test_data)
-    end_time = time.time() - start_time
-    while result.json() and not result.json()[0].get("response"):
-        result = requests.post(url, json={})
-    assert end_time <= 0.4, "Unsufficient run time"
-    print(f"...\nAverage response time is {end_time}")
-
-# @allure.description("""Simple execution test""")
-# def test_execution():
-#     video_path = "http://files:3000/file?file=file_227.mp4"
-#     gold_result = [{'video_captioning_chapters': "[{'sentence': 'Intro.', 'timestamp': [0.0, 10.727636363636364]}, {'sentence': 'Showing impressive award combinations.', 'timestamp': [10.727636363636364, 30.3949696969697]}, {'sentence': 'Discussing who won an Oscar and a gold medal.', 'timestamp': [30.3949696969697, 59.002]}]\n"}]
-#     test_data = { "video_paths": [video_path], "video_durations": [59], "video_types": ['.mp4']} 
-#     result = requests.post(url, json=test_data)
-#     print(f"Sent video {video_path}\nGot result {result.json()}")
-#     assert True
+    test_data = { 
+        "video_paths": [video_path], 
+        "video_durations": [59], 
+        "video_types": ['.mp4']
+    }    
+    _, avg_time = _call_service(test_data)
+    assert avg_time <= 0.4, "Unsufficient run time"
+    print(f"...\nAverage response time is {avg_time}")
     
 
 if __name__ == "__main__":
