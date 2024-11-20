bef641161ec0a834d3669a48cf3b2deccd6c5858
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2024-11-20 14:59:34 +0400

    vidchapters fixed for parallel requests

--- annotators/vidchapters_service/server.py
+++ annotators/vidchapters_service/server.py
@@ -2,13 +2,12 @@ import logging
 import os
 import time
 from itertools import zip_longest
-import asyncio
 import json
+import uuid
 
 import sentry_sdk
 from urllib.request import URLopener
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 from typing import List
@@ -19,44 +18,43 @@ import pickle
 
 import subprocess
 
-
 import sys
 sys.path.append('/src/aux_files/VidChapters')
 from args import MODEL_DIR
-
 
 CAP_ERR_MSG = "The file format is not supported"
 CHECKPOINTS = "/src/aux_files/checkpoint_vidchapters"
 MODEL_PATH = "/src/aux_files/captioning_model.pth"
 DATA_DIR = "/src/aux_files/data/video_captioning"
 ASR_MODEL = "/src/aux_files/TOFILL/large-v2.pt"
-DEVICE='cpu'
+DEVICE = 'cpu'
 
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"))
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
-# create asr whisper model here
-
-asr_model = whisper.load_model('/src/aux_files/large-v2.pt', device='cpu', download_root='/src/aux_files/TOFILL')
+# Create ASR Whisper model here
+asr_model = whisper.load_model(
+    '/src/aux_files/large-v2.pt', device='cpu', download_root='/src/aux_files/TOFILL'
+)
 
 logging.getLogger("werkzeug").setLevel("WARNING")
 
-STATE_FILE = "task_state.json"
+TASKS_DIR = "tasks"
+os.makedirs(TASKS_DIR, exist_ok=True)
 
 
-def read_task_state():
-    try:
-        with open(STATE_FILE, "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return {"state": ""}
-
-    
-def write_task_state(state):
-    with open(STATE_FILE, "w") as file:
-        json.dump(state, file)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class VideoPayload(BaseModel):
@@ -69,37 +67,44 @@ def generate_asr(video_path, asr_output_path):
     logger.info("ASR captioning")
     try:
         asr = asr_model.transcribe(video_path)
-        # logger.info("ASR.model")
-        align_model, metadata = whisperx.load_align_model(language_code='en', device = DEVICE, model_dir=os.path.join('/src/aux_files', MODEL_DIR))
+        align_model, metadata = whisperx.load_align_model(
+            language_code='en', device=DEVICE, model_dir=os.path.join('/src/aux_files', MODEL_DIR)
+        )
         audio = whisperx.load_audio(video_path)
-        # logger.info("Whisperx.load_audio", audio)
-        aligned_asr = whisperx.align(asr["segments"], align_model, metadata, audio, DEVICE, return_char_alignments=False)
-        # logger.info("Aligned", aligned_asr)
+        aligned_asr = whisperx.align(
+            asr["segments"],
+            align_model,
+            metadata,
+            audio,
+            DEVICE,
+            return_char_alignments=False,
+        )
         pickle.dump(aligned_asr, open(asr_output_path, 'wb'))
     except Exception as e:
         logger.warning(f"str{e}, {type(e)=}")
-    
+
     return asr_output_path
 
 
 def gen_video_caption(video_path, asr_caption):
-    # takes 1 min
     path_2_demo = '/src/aux_files/VidChapters/demo_vid2seq.py'
     command = [
-        "python", 
+        "python",
         path_2_demo,
         f'--load={MODEL_PATH}',
         f'--video_example={video_path}',
         f'--asr_example={asr_caption}',
-        "--combine_datasets", "chapters",
-        "--device", "cpu"
+        "--combine_datasets",
+        "chapters",
+        "--device",
+        "cpu",
     ]
     try:
         result = subprocess.run(command, capture_output=True, text=True)
-        # logger.info(result.stdout)
     except Exception as e:
         logger.warning(f"str{e}, {type(e)=}")
-    return result.stdout
+        result = None
+    return result.stdout if result else "Error"
 
 
 def get_answer(video_path, asr_output_path):
@@ -110,10 +115,30 @@ def get_answer(video_path, asr_output_path):
     return video_caption
 
 
-async def subinfer(paths, durations, types):
-    write_task_state({"state": "scheduled"})
+def write_task_status(task_id, status, result=None):
+    task_file = os.path.join(TASKS_DIR, f"{task_id}.json")
+    task_status = {
+        "task_id": task_id,
+        "status": status,
+        "result": result,
+    }
+    with open(task_file, "w", encoding="utf-8") as f:
+        json.dump(task_status, f)
+
+
+def read_task_status(task_id):
+    task_file = os.path.join(TASKS_DIR, f"{task_id}.json")
+    if os.path.exists(task_file):
+        with open(task_file, "r", encoding="utf-8") as f:
+            return json.load(f)
+    else:
+        return None
+
+
+def subinfer(task_id, paths, durations, types):
+    write_task_status(task_id, "pending")
     responses = []
-    
+
     for path, duration, atype in zip_longest(paths, durations, types):
         logger.info(f"Processing batch at vidchapters annotator: {path}")
         if '=' in path:
@@ -134,56 +159,69 @@ async def subinfer(paths, durations, types):
         try:
             logger.info(f"Scanning DATA_DIR ({DATA_DIR}) for files...")
             for i in os.listdir(DATA_DIR):
-                # i is a filename without path
                 logger.info("Scanning finished successfully, files found, starting inference...")
                 break
             else:
-                CAP_ERR_MSG = "No files for inference found in DATA_DIR"
-                raise Exception(CAP_ERR_MSG)
-            
-            asr_output_path = os.path.join(DATA_DIR, i.split(".")[0]+'_asr')
+                cap_err_msg = "No files for inference found in DATA_DIR"
+                raise Exception(cap_err_msg)
+
+            asr_output_path = os.path.join(DATA_DIR, i.split(".")[0] + '_asr')
             video_path = os.path.join(DATA_DIR, i)
             video_caption = get_answer(video_path, asr_output_path)
-            responses.append({"video_captioning_chapters": video_caption})
-        except Exception:
-            logger.info(f"An error occurred in vidchapters-service: {CAP_ERR_MSG}")
-            responses.append(
-                [{"video_captioning_chapters": "Error occured"}]
-            )
+            responses.append({
+                "video_type": atype,
+                "video_duration": duration,
+                "video_path": path,
+                "caption": video_caption,
+            })
+        except Exception as e:
+            logger.info(f"An error occurred in vidchapters-service: {CAP_ERR_MSG}, {e}")
+            responses.append({
+                "video_type": atype,
+                "video_duration": duration,
+                "video_path": path,
+                "caption": "Error occurred",
+            })
 
     logger.info(f"VIDCHAPTERS_SERVICE RESPONSE: {responses}")
-    write_task_state({"state": "done", "response": responses})
-        
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    status = (
+        "failed" if any(resp.get("caption") == "Error occurred" for resp in responses) else "completed"
+    )
+    write_task_status(task_id, status, responses)
 
 
 @app.post("/respond")
-async def respond(payload: VideoPayload):
+def respond(payload: VideoPayload, background_tasks: BackgroundTasks):
     st_time = time.time()
-    task_state = read_task_state()
-    logger.info(f"Task state: {task_state}")
-    logger.info(f'Paths: {payload.video_paths}')
+    task_id = str(uuid.uuid4())
+    write_task_status(task_id, "pending")
+    background_tasks.add_task(
+        subinfer, task_id, payload.video_paths, payload.video_durations, payload.video_types
+    )
 
-    responses = []
-    
-    if not task_state.get("state") and payload.video_paths:
-        asyncio.create_task(subinfer(payload.video_paths, payload.video_durations, payload.video_types))
-        responses.append({"state": "scheduled"})
-                
-    if task_state.get("state") == "scheduled":
-        responses.append(task_state)
-    elif task_state.get("state") == "done":
-        responses.append(task_state)
-        write_task_state({"state": ""})
+    all_tasks = []
+    for filename in os.listdir(TASKS_DIR):
+        if filename.endswith(".json"):
+            task_file_path = os.path.join(TASKS_DIR, filename)
+            try:
+                with open(task_file_path, "r") as f:
+                    task_data = json.load(f)
+                    task_info = {
+                        "task_id": task_data.get("task_id"),
+                        "status": task_data.get("status"),
+                        "result": task_data.get("result"),
+                    }
+                    all_tasks.append(task_info)
+            except json.JSONDecodeError:
+                logger.error(f"Error decoding JSON in file: {task_file_path}")
+            except Exception as e:
+                logger.error(f"An error occurred while processing file {task_file_path}: {e}")
 
     total_time = time.time() - st_time
+    cur_status_str = "".join(
+        f"id: {task['task_id']}: {task['status']}, "
+        f"caption: {task['result'] or 'N/A'} \n"
+        for task in all_tasks
+    )
     logger.info(f"service exec time: {total_time:.3f}s")
-    return jsonable_encoder(responses)
+    return [{"task_id": task_id, "status": "pending", "all_status": cur_status_str}]
